7b8ab99e81d2e019263bf9e9e4d88fb3f0ad9dc3
Cristhian892 <cristhian.jimenezcampos@ucr.ac.cr>
2025-06-29 19:22:04 -0600

    Codigo Interfaz

--- Interfaz.java
+++ Interfaz.java
@@ -1,8 +1,15 @@
+import java.awt.BorderLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
+
+import javafx.scene.layout.Border;
 
 public class Interfaz extends JFrame{
 
@@ -35,5 +42,38 @@ public class Interfaz extends JFrame{
 
     }
 
+
+    private crearComponente(){
+        //Busqueda
+        JPanel panelSuperior = new JPanel(new flowLayout());
+        campoBusqueda = new JTextField(30);
+        botonBuscar = new JTextField("Buscar por ID");
+        botonCargar = JButton("Cargar Datos");
+
+        panelSuperior.add(new JLabel("Buscar por ID:"));
+        panelSuperior.add(campoBusqueda);
+        panelSuperior.add(botonBuscar);
+        panelSuperior.add(botonCargar);
+        add(panelSuperior, BorderLayout.NORTH);
+
+
+        modeloTabla = new DefaultTableModel();
+        modeloTabla.addColumn("ID");
+        modeloTabla.addColumn("Nombre");
+        modeloTabla.addColumn("Historial");
+
+        tablaMascotas = new DefaultTableModel(modeloTabla);
+        JScrollPane scrollPane = new JScrollPane(tablaMascotas);
+        add(scrollPane, BorderLayout.CENTER);
+
+        //panel inferior
+        JPanel panelInferior = new JPanel();
+        panelInferior.add(new JLabel("Mascotas registradas : 0"));
+        add(panelInferior, BorderLayout.SOUTH);
+
+        botonBuscar.addActionListener(e -> buscarPorID());
+        botonCargar.addActionListener(e -> cargarDatos());
+        campoBusqueda.addActionListener(e -> buscarPorID());
+    }
     
 }
